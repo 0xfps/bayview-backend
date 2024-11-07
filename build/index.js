b0848371aebed99c0a89c37b2b1e1bafd7c7cf68
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-11-07 09:03:38 +0100

    feat: server side.

--- build/index.js
+++ build/index.js
@@ -1,3 +1,31 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
 require("./db/index");
+dotenv_1.default.config();
+const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 8000;
+const app = (0, express_1.default)();
+// @todo Configure cors at the end of the project.
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded());
+// GET Routes.
+// ---
+// POST Routes.
+// ---
+app.get("/", function (req, res) {
+    res.send({
+        status: 200,
+        msg: "OK",
+        data: ["Hola, amigo!"]
+    });
+});
+app.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}.`);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
 import "./db/index"
+
+dotenv.config()
+const PORT = process.env.PORT ?? 8000
+
+const app = express()
+
+// @todo Configure cors at the end of the project.
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
+
+// GET Routes.
+// ---
+
+// POST Routes.
+// ---
+
+app.get("/", function (req, res) {
+    res.send({
+        status: 200,
+        msg: "OK",
+        data: ["Hola, amigo!"]
+    })
+})
+
+app.listen(PORT, function () {
+    console.log(`Listening on port ${PORT}.`)
+})
